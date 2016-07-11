28f08723b1a6b5effc041d66d19a7c857317625d
SmallJoker <mk939@ymail.com>
2016-07-11 16:32:26 +0200

    Rewrite the complete mod

--- init.lua
+++ init.lua
@@ -1,148 +1,136 @@
 -- flatgen mod for 100% flat maps - created by Krock
 -- License: WTFPL
-local flatgen = {}
+local terrain_file = minetest.get_worldpath() .."/flatgen_terrain.txt"
 
--- Default, grass cover, dirt in between, stone under it
-flatgen.cover	= "default:dirt_with_grass"
-flatgen.under	= "default:dirt"
-flatgen.ground	= "default:stone"
+-- Y position where the terrain starts to generate
+local generator_start_y = 1
+-- Default settings: grass, dirt, dirt, dirt with stone ground
+local layers = {
+	{ "default:dirt_with_grass", 1 },
+	{ "default:dirt", 3 },
+	{ "default:stone", nil } -- nil = no limit
+	-- Fake ignore to generate whenever you want: { "ignore_me", 1 }
+}
 
---[[ Templates:
-- Sand everywhere
-	flatgen.cover	= "default:sand"
-	flatgen.under	= "default:sand"
-	flatgen.ground	= "default:sand"
-	
-- Desert sand, sand, sandstone
-	flatgen.cover	= "default:desert_sand"
-	flatgen.under	= "default:sand"
-	flatgen.ground	= "default:sandstone"
 
-- Wool, wool, obsidian
-	flatgen.cover	= "wool:white"
-	flatgen.under	= "wool:white"
-	flatgen.ground	= "default:obsidian"
-]]
+minetest.set_mapgen_params({
+	mgname = "singlenode",
+	water_level = -31000,
+	flags = "nolight"
+})
 
-flatgen.cover_y		= 1		-- Y position of the cover node [1]
-flatgen.depth		= 3		-- Depth of the "under" node [3]
-flatgen.limit_y		= true	-- False = map generation stops at 100m under cover_y
+local function readOrCreateTerrainSettings()
+	local file = io.open(terrain_file, "r")
+	if file then
+		-- Load existing world file
+		local data = minetest.deserialize(file:read("*all"))
+		file:close()
+		assert(data, "[flatgen] Can not deserialize the terrain file")
+		layers = data
+		return
+	end
+	-- Copy default params to the world file
+	file = io.open(terrain_file, "w")
+	file:write(minetest.serialize(layers))
+	file:close()
+end
 
-minetest.register_on_mapgen_init(function(mgparams)
-	if mgparams.mgname ~= "singlenode" then
-		minetest.set_mapgen_params({mgname="singlenode"})
+readOrCreateTerrainSettings()
+
+minetest.after(0, function()
+	-- Transform node strings to content ID
+	for i, v in ipairs(layers) do
+		assert(minetest.registered_nodes[v[1]], "[flatgen] Unknown node: "..v[1])
+		v[1] = minetest.get_content_id(v[1])
 	end
 end)
 
-function vector.floor(v)
-	return {
-		x = math.floor(v.x),
-		y = math.floor(v.y),
-		z = math.floor(v.z)
-	}
-end
 
 minetest.register_chatcommand("regenerate", {
-	description = "Regenerates a (<size>*8)^3 chunk around you",
+	description = "Regenerates a <size>^3 nodes around you",
 	params = "<size>",
 	privs = {server=true},
 	func = function(name, param)
-		local size = tonumber(param) or 1
-		
-		if size > 8 then
-			size = 8 -- Limit: 8*8 -> 64
-		elseif size < 1 then
-			return false, "Nothing to do."
+		local size = tonumber(param) or 0
+
+		if size < 10 then
+			return false, "Please submit a size number >= 10"
 		end
-		
-		size = size * 8
+
+		size = math.floor(size / 2)
 		local player = minetest.get_player_by_name(name)
-		local pos = vector.floor(vector.divide(player:getpos(), size))
-		local minp = vector.multiply(pos, size)
-		local maxp = vector.add(minp, size - 1)
-		
-		flatgen.generate(minp, maxp, math.random(0, 9999), true)
+		local pos = vector.round(player:getpos())
+
+		flatgen_generate(
+			vector.subtract(pos, size),
+			vector.add(pos, size),
+			0, true
+		)
 		return true, "Done!"
 	end
 })
 
-function flatgen.generate(minp, maxp, seed, regen)
-	local cover, depth, limit = flatgen.cover_y, flatgen.depth, flatgen.limit_y
-	if minp.y > cover then
-		return
+local c_air = minetest.get_content_id("air")
+function flatgen_getNode(y, regenerate)
+	if y > generator_start_y then
+		return c_air
 	end
-	if limit then
-		limit = cover - 100
-		if maxp.y < limit then
-			return
+
+	local depth = generator_start_y
+	for i, v in ipairs(layers) do
+		if not v[2] or depth - v[2] < y then
+			return v[1]
 		end
+		depth = depth - v[2]
 	end
-	
-	local c_cover = minetest.get_content_id(flatgen.cover)
-	local c_under = minetest.get_content_id(flatgen.under)
-	local c_ground = minetest.get_content_id(flatgen.ground)
-	local c_ignore = minetest.get_content_id("ignore_me")
+	return regenerate and c_air --or nil
+end
+
+function flatgen_generate(minp, maxp, seed, regenerate)
+	if minp.y > generator_start_y then
+		return -- Area outside of the generation area
+	end
+
+	if not flatgen_getNode(maxp.y) then
+		return -- No node defined for that position or below
+	end
 
 	local vm, emin, emax
-	if regen then
+	if regenerate then -- Load regular VManip to regenerate area
 		vm = minetest.get_voxel_manip()
 		emin, emax = vm:read_from_map(minp, maxp)
-	else
+	else -- Load VManip from map generator
 		vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
 	end
-	
+
 	local area = VoxelArea:new{MinEdge=emin, MaxEdge=emax}
 	local data = vm:get_data()
-	
-	if regen then
-		local air = minetest.get_content_id("air")
-		for z = minp.z, maxp.z do
-		for y = minp.y, maxp.y do
-			local vi = area:index(minp.x, y, z)
-			for x = minp.x, maxp.x do
-				data[vi] = air
-				vi = vi + 1
-			end
-		end
-		end
-	end
-	
+
 	for z = minp.z, maxp.z do
 	for y = minp.y, maxp.y do
 		local vi = area:index(minp.x, y, z)
+		local node = flatgen_getNode(y, regenerate)
 		for x = minp.x, maxp.x do
-			if y > cover then
-				-- do nothing :)
-			elseif y == cover then
-				data[vi] = c_cover
-			elseif y >= cover - depth then
-				data[vi] = c_under
-			elseif limit then
-				if y > limit then
-					data[vi] = c_ground
-				elseif y <= limit and y >= limit - 10 then
-					data[vi] = c_ignore
-				end
-			else
-				data[vi] = c_ground
+			if node then
+				data[vi] = node
 			end
 			vi = vi + 1
 		end
 	end
 	end
-	
+
 	vm:set_data(data)
-	if not regen then
+	if not regenerate then
 		vm:set_lighting({day=0, night=0})
 	end
 	vm:calc_lighting()
 	vm:write_to_map(data)
-	if regen then
+	if regenerate then
 		vm:update_map()
 	end
 end
 
-table.insert(minetest.registered_on_generateds, 1, flatgen.generate)
+table.insert(minetest.registered_on_generateds, 1, flatgen_generate)
 
 minetest.register_node(":ignore_me", {
 	description = "You hacker you!",
